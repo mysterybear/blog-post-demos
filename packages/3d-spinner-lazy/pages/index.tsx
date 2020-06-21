510d42c38163c2be851e849cc7742d5fadda2e8d
Thomas O'Neill <tom@bearjam.dev>
2020-06-21 15:46:15 +0100

    yup

--- packages/3d-spinner-lazy/pages/index.tsx
+++ packages/3d-spinner-lazy/pages/index.tsx
@@ -1,5 +1,7 @@
 import { useInView } from "react-intersection-observer"
 import dynamic from "next/dynamic"
+import { useState } from "react"
+import { motion } from "framer-motion"
 
 const Spinner3D = dynamic(() => import("../components/spinner-3d"))
 
@@ -14,15 +16,42 @@ const Content = () => {
 export default () => {
   const [inViewRef, inView] = useInView({
     triggerOnce: true,
-    rootMargin: `-150px 0px`,
+    rootMargin: `-200px 0px`,
   })
+  const [loading, setLoading] = useState(false)
+  const load = () => {
+    if (loading) return
+    setLoading(true)
+    setTimeout(() => void setLoading(false), 3000)
+  }
   return (
     <div>
       <Content />
       <Content />
-      <div ref={inViewRef} className="relative w-full h-64 border">
-        {inView && <Spinner3D />}
-      </div>
+      <section
+        ref={inViewRef}
+        className="flex justify-center items-center h-64 w-full relative"
+      >
+        {inView && (
+          <motion.div
+            key="spinner"
+            className="absolute w-full h-full"
+            initial={{ opacity: 0 }}
+            animate={loading ? { opacity: 1 } : { opacity: 0 }}
+          >
+            <Spinner3D />
+          </motion.div>
+        )}
+        <motion.button
+          key="button"
+          className="rounded-md shadow-md focus:outline-none px-5 py-2 bg-indigo-500 z-50"
+          onClick={load}
+          initial={{ opacity: 1 }}
+          animate={loading ? { opacity: 0 } : { opacity: 1 }}
+        >
+          Load
+        </motion.button>
+      </section>
       <Content />
     </div>
   )
