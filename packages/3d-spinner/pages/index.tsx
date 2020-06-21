83b3bf8c0b093935be104ca7f0493df0927c2cf5
Thomas O'Neill <tom@bearjam.dev>
2020-06-21 16:05:12 +0100

    foobar

--- packages/3d-spinner/pages/index.tsx
+++ packages/3d-spinner/pages/index.tsx
@@ -2,9 +2,11 @@ import Spinner3D from "../components/spinner-3d"
 
 export default function IndexPage() {
   return (
-    <div className="absolute w-full h-full bg-indigo-300">
-      <Spinner3D />
-      <h1>Hello</h1>
+    <div className="absolute w-full h-full">
+      <h1 className="absolute">Some Text</h1>
+      <div className="absolute w-full h-full bg-indigo-300">
+        <Spinner3D />
+      </div>
     </div>
   )
 }
